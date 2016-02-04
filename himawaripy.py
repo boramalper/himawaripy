ce1b1478c376bfc8085f65756dfdd27740d5daa1
Brandon <brandonskerritt51@gmail.com>
2016-02-04 19:43:10 +0000

    Auto refreshes every hour
    1 hour is 3600 seconds, this is on an infinite loop which auto refreshes every hour. 
    uses from time import sleep to save memory

--- himawaripy.py
+++ himawaripy.py
@@ -12,6 +12,8 @@ from PIL import Image
 
 from utils import get_desktop_environment
 
+from time import sleep
+
 # Configuration
 # =============
 
@@ -24,51 +26,53 @@ output_file = expanduser("~/.himawari/himawari-latest.png")
 # ==============================================================================
 
 def main():
-    width = 550
-    height = 550
-
-    print("Updating...")
-    with urlopen("http://himawari8-dl.nict.go.jp/himawari8/img/D531106/latest.json") as latest_json:
-        latest = strptime(loads(latest_json.read().decode("utf-8"))["date"], "%Y-%m-%d %H:%M:%S")
-
-    print("Latest version: {} GMT\n".format(strftime("%Y/%m/%d/%H:%M:%S", latest)))
-
-    url_format = "http://himawari8.nict.go.jp/img/D531106/{}d/{}/{}_{}_{}.png"
-
-    png = Image.new('RGB', (width*level, height*level))
-
-    print("Downloading tiles: 0/{} completed".format(level*level), end="\r")
-    for x in range(level):
-        for y in range(level):
-            with urlopen(url_format.format(level, width, strftime("%Y/%m/%d/%H%M%S", latest), x, y)) as tile_w:
-                tiledata = tile_w.read()
-
-            tile = Image.open(BytesIO(tiledata))
-            png.paste(tile, (width*x, height*y, width*(x+1), height*(y+1)))
-
-            print("Downloading tiles: {}/{} completed".format(x*level + y + 1, level*level), end="\r")
-    print("\nDownloaded\n")
-
-    makedirs(split(output_file)[0], exist_ok=True)
-    png.save(output_file, "PNG")
-
-    de = get_desktop_environment()
-    if de in ["gnome", "unity", "cinnamon"]:
-        # Because of a bug and stupid design of gsettings, see http://askubuntu.com/a/418521/388226
-        if de == "unity":
-            call(["gsettings", "set", "org.gnome.desktop.background", "draw-background", "false"])
-        call(["gsettings", "set", "org.gnome.desktop.background", "picture-uri", "file://" + output_file])
-        call(["gsettings", "set", "org.gnome.desktop.background", "picture-options", "scaled"])
-    elif de == "mate":
-        call(["gconftool-2", "-type", "string", "-set", "/desktop/gnome/background/picture_filename", '"{}"'.format(output_file)])
-    elif de == "xfce4":
-        call(["xfconf-query", "--channel", "xfce4-desktop", "--property", "/backdrop/screen0/monitor0/image-path", "--set", output_file])
-    elif de == "lxde":
-        call(["display", "-window", "root", output_file])
-    else:
-        exit("Your desktop environment '{}' is not supported.".format(de))
-
-    print("Done!\n")
+    while True:
+        width = 550
+        height = 550
+    
+        print("Updating...")
+        with urlopen("http://himawari8-dl.nict.go.jp/himawari8/img/D531106/latest.json") as latest_json:
+            latest = strptime(loads(latest_json.read().decode("utf-8"))["date"], "%Y-%m-%d %H:%M:%S")
+    
+        print("Latest version: {} GMT\n".format(strftime("%Y/%m/%d/%H:%M:%S", latest)))
+    
+        url_format = "http://himawari8.nict.go.jp/img/D531106/{}d/{}/{}_{}_{}.png"
+    
+        png = Image.new('RGB', (width*level, height*level))
+    
+        print("Downloading tiles: 0/{} completed".format(level*level), end="\r")
+        for x in range(level):
+            for y in range(level):
+                with urlopen(url_format.format(level, width, strftime("%Y/%m/%d/%H%M%S", latest), x, y)) as tile_w:
+                    tiledata = tile_w.read()
+    
+                tile = Image.open(BytesIO(tiledata))
+                png.paste(tile, (width*x, height*y, width*(x+1), height*(y+1)))
+    
+                print("Downloading tiles: {}/{} completed".format(x*level + y + 1, level*level), end="\r")
+        print("\nDownloaded\n")
+    
+        makedirs(split(output_file)[0], exist_ok=True)
+        png.save(output_file, "PNG")
+    
+        de = get_desktop_environment()
+        if de in ["gnome", "unity", "cinnamon"]:
+            # Because of a bug and stupid design of gsettings, see http://askubuntu.com/a/418521/388226
+            if de == "unity":
+                call(["gsettings", "set", "org.gnome.desktop.background", "draw-background", "false"])
+            call(["gsettings", "set", "org.gnome.desktop.background", "picture-uri", "file://" + output_file])
+            call(["gsettings", "set", "org.gnome.desktop.background", "picture-options", "scaled"])
+        elif de == "mate":
+            call(["gconftool-2", "-type", "string", "-set", "/desktop/gnome/background/picture_filename", '"{}"'.format(output_file)])
+        elif de == "xfce4":
+            call(["xfconf-query", "--channel", "xfce4-desktop", "--property", "/backdrop/screen0/monitor0/image-path", "--set", output_file])
+        elif de == "lxde":
+            call(["display", "-window", "root", output_file])
+        else:
+            exit("Your desktop environment '{}' is not supported.".format(de))
+    
+        print("Done!\n")
+    sleep(3600)
 
 if __name__ == "__main__":
     main()
