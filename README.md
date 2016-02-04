4a995cec67f300b832077ba047653defb69e6991
Bora M. Alper <bora@boramalper.org>
2016-02-04 18:22:58 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# himawaripy
+*Put near-realtime picture of Earth as your desktop background*
+
+himawaripy is a Python 3 script that fetches near-realtime (10 minutes delayed)
+picture of Earth as its taken by
+[Himawari 8 (ひまわり8号)](https://en.wikipedia.org/wiki/Himawari_8) and sets it
+as your desktop background.
+
+Set a cronjob that runs in every 10 minutes to automatically get the
+near-realtime picture of Earth.
+
+Tested on Ubuntu 15.10.
+
+## Configuration
+You can configure the level of detail, by modifying the script. You can set the
+global variable `level` to `4`, `8`, `16`, or `20` to increase the quality (and
+thus the file size as well). Please keep in mind that it will also take more
+time to download the tiles.
+
+## Installation
+    cd ~
+    git clone https://github.com/boramalper/himawaripy.git
+    
+    # configure
+    cd ~/himawaripy
+    vi himawaripy.py
+    
+    # test whether it's working
+    python3 himawaripy.py
+    
+    # set up a cronjob
+    crontab -e
+    # Add the line:
+    # */10 * * * * python3 /home/USERNAME/himawaripy/himawaripy.py
+    
+## Example
+![Earth, as 2016/02/04/13:30:00 GMT](http://i.imgur.com/4XA6WaM.jpg)
+    
+## Attributions
+Thanks to *[MichaelPote](https://github.com/MichaelPote)* for the [initial
+implementation](https://gist.github.com/MichaelPote/92fa6e65eacf26219022) using
+Powershell Script.
+
+Thanks to *[Charlie Loyd](https://github.com/celoyd)* for image processing logic
+([hi8-fetch.py](https://gist.github.com/celoyd/39c53f824daef7d363db)).
+
+Obviously, thanks to the Japan Meteorological Agency for opening these pictures
+to public.
---
+++ himawaripy.py
@@ -0,0 +1,55 @@
+from json import loads
+from io import BytesIO
+from urllib.request import urlopen
+from time import strptime, strftime
+from os import system
+from os.path import expanduser
+
+from PIL import Image
+
+# Configuration
+# =============
+
+# Increases the quality and the size. Possible values: 4, 8, 16, 20
+level = 4 
+
+# ==============================================================================
+
+def main():
+    width = 550
+    height = 550
+
+    print("Updating...")
+    with urlopen("http://himawari8-dl.nict.go.jp/himawari8/img/D531106/latest.json") as latest_json:
+        latest = strptime(loads(latest_json.read().decode("utf-8"))["date"], "%Y-%m-%d %H:%M:%S")
+    print("Latest version: {} GMT\n".format(strftime("%Y/%m/%d/%H:%M:%S", latest)))
+
+    url_format = "http://himawari8.nict.go.jp/img/D531106/{}d/{}/{}_{}_{}.png"
+
+    png = Image.new('RGB', (width*level, height*level))
+
+    print("Downloading tiles: 0/{} completed".format(level*level), end="\r")
+    for x in range(level):
+        for y in range(level):
+            with urlopen(url_format.format(level, width, strftime("%Y/%m/%d/%H%M%S", latest), x, y)) as tile_w:
+                tiledata = tile_w.read()
+    
+            tile = Image.open(BytesIO(tiledata))    
+            png.paste(tile, (width*x, height*y, width*(x+1), height*(y+1)))
+        
+            print("Downloading tiles: {}/{} completed".format(x*level + y + 1, level*level), end="\r")
+    print("\nDownloaded\n")
+
+    png.save(expanduser("~/.himawari-latest.png"), "PNG")
+
+    # Because of a bug and stupid design of gsettings, see http://askubuntu.com/a/418521/388226
+    system("gsettings set org.gnome.desktop.background draw-background false \
+            && gsettings set org.gnome.desktop.background picture-uri file://"
+            + expanduser("~/.himawari-latest.png") + 
+            " && gsettings set org.gnome.desktop.background picture-options scaled")
+
+    print("Done!\n")
+    
+if __name__ == "__main__":
+    main()
+
