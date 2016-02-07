388b73dd0cb7531152f8cffb06405585328dec28
Daniel Schmitz <dr1s@users.noreply.github.com>
2016-02-08 00:09:34 +0100

    Improvements / Merge remote-tracking branch 'boramalper/master' into time_offset
    # Conflicts:
    #	README.md
    #	himawaripy.py
    #      config.py
    #      setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name='himawaripy',
+    version='1.1',
+    url='https://github.com/boramalper/himawaripy',
+    author='Mert Bora Alper',
+    author_email='bora@boramalper.org',
+    license='MIT',
+    description='Put near-realtime picture of Earth as your desktop background',
+    long_description='himawaripy is a Python 3 script that fetches near-realtime (10 minutes delayed) picture of Earth '
+                     'as its taken by Himawari 8 (ひまわり8号) and sets it as your desktop background.',
+    install_requires=["pillow","python-dateutil","datetime","pytz","tzlocal"],
+    packages=find_packages(),
+    entry_points={'console_scripts': ['himawaripy=himawaripy.himawaripy:main']},
+)
