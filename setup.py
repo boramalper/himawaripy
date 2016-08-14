2bd487bcb85851639aca78946901feee5a36bf71
Bob Evans <n1be@github.com>
2016-08-14 18:48:32 -0400

    More reliably find local timezone
    Signed-off-by: Bob Evans <n1be@github.com>

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='Put near-realtime picture of Earth as your desktop background',
     long_description='himawaripy is a Python 3 script that fetches near-realtime (10 minutes delayed) picture of Earth '
                      'as its taken by Himawari 8 (ひまわり8号) and sets it as your desktop background.',
-    install_requires=["appdirs", "pillow", "python-dateutil", "pytz", "tzlocal"],
+    install_requires=["appdirs", "pillow", "python-dateutil", "pytz"],
     packages=find_packages(),
     entry_points={'console_scripts': ['himawaripy=himawaripy.himawaripy:main']},
 )
