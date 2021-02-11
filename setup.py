844fc91c9208a3d13c3be979c12fe63dab335888
xiaochi <wxiaochi@qq.com>
2021-02-11 12:29:34 +0800

    pypiwin32 if windows

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup, find_packages
+import platform
 
 with open("README.md", "r", encoding='UTF-8') as f:
     long_description = f.read()
+
+install_requires = ["appdirs", "pillow", "python-dateutil"]
+sys = platform.system()
+if sys == "Windows":
+    install_requires.append("pypiwin32")
 
 setup(
     name="himawaripy",
@@ -14,7 +20,7 @@ setup(
                 "as its taken by Himawari 8 (ひまわり8号) and sets it as your desktop background.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=["appdirs", "pillow", "python-dateutil"],
+    install_requires=install_requires,
     packages=find_packages(),
     entry_points={"console_scripts": ["himawaripy=himawaripy.__main__:main"]},
 )
