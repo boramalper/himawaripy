3baa3293fc8c6c44ef4f8f353f729cf67c9f53d1
xiaochi <wxiaochi@qq.com>
2021-02-11 12:31:41 +0800

    center

--- himawaripy/utils.py
+++ himawaripy/utils.py
@@ -13,7 +13,7 @@ def set_wallpaper_from_bmp(bmp_path):
     reg_key = win32api.RegOpenKeyEx(
         win32con.HKEY_CURRENT_USER, "Control Panel\\Desktop", 0, win32con.KEY_SET_VALUE)
     #最后的参数:2拉伸,0居中,6适应,10填充,0平铺
-    win32api.RegSetValueEx(reg_key, "WallpaperStyle", 0, win32con.REG_SZ, "2")
+    win32api.RegSetValueEx(reg_key, "WallpaperStyle", 0, win32con.REG_SZ, "0")
     #最后的参数:1表示平铺,拉伸居中等都是0
     win32api.RegSetValueEx(reg_key, "TileWallpaper", 0, win32con.REG_SZ, "0")
     #刷新桌面
@@ -274,4 +274,4 @@ def fetch_envvar(varname):
         print("Could NOT retrieve env. var. {}".format(varname))
 
 if __name__ == "__main__":
-    set_background("D:\data\hima\himawari-20210206T121000.png")
+    set_background("e:\data\hima\himawari-20210211T021000.png")
